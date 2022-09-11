f1037feababbdfc99bfcded0367f83f5f87ba7e7
Grant Rettke <grant@wisdomandwonder.com>
2022-09-11 18:25:45 -0500

    Auto-inserts base file template

---
+++ flycheck-org-vale.el
@@ -0,0 +1,6 @@
+;;; flycheck-org-vale.el --- Org syntax-aware Vale linter using Flycheck  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Wisdom and Wonder
+
+;; Author: Grant Rettke <grant@wisdomandwonder.com>
+;; Keywords: convenience, languages, tools, 
